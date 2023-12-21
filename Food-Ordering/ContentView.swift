8bdbc229f3b921679508091e73633cfce9d9e478
ashleykcheng <ashleykcheng@gmail.com>
2023-12-21 10:14:37 -0800

    Initial Commit

---
+++ Food-Ordering/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food-Ordering
+//
+//  Created by Ashley on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food-Ordering/Food_OrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_OrderingApp.swift
+//  Food-Ordering
+//
+//  Created by Ashley on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_OrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
